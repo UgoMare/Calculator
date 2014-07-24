c2c169dedfa9b137ac16d062db140693f33bac73
sqx <ugo.mare.epitech@gmail.com>
2014-07-25 01:51:36 +0200

    Main.storyboard + VC.m

--- Calculator/ViewController.h
+++ Calculator/ViewController.h
@@ -9,5 +9,24 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UILabel *resultLabel;
+- (IBAction)button1;
+- (IBAction)button2;
+- (IBAction)button3;
+- (IBAction)button4;
+- (IBAction)button5;
+- (IBAction)button6;
+- (IBAction)button7;
+- (IBAction)button8;
+- (IBAction)button9;
+- (IBAction)button0;
+- (IBAction)buttonEqual;
+- (IBAction)buttonPlus;
+- (IBAction)buttonMinus;
+- (IBAction)buttonMult;
+- (IBAction)buttonDiv;
+- (IBAction)buttonC;
+
+-(void) editResult:(NSString*) idButton;
 
 @end
--- Calculator/ViewController.m
+++ Calculator/ViewController.m
@@ -26,4 +26,77 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)editResult:(NSString*) idButton {
+    if ([idButton isEqual:@"+"] || [idButton isEqual:@"-"] || [idButton isEqual:@"*"] || [idButton isEqual:@"/"]){
+        NSLog(@"math op");
+    }
+    if ([self.resultLabel.text  isEqual: @"0"]){
+        self.resultLabel.text = [NSString stringWithFormat:@"%@", idButton];
+    }
+    else{
+        self.resultLabel.text = [NSString stringWithFormat:@"%@%@", self.resultLabel.text, idButton];
+    }
+}
+
+- (IBAction)button1 {
+    [self editResult:@"1"];
+}
+
+- (IBAction)button2 {
+    [self editResult:@"2"];
+}
+
+- (IBAction)button3 {
+    [self editResult:@"3"];
+}
+
+- (IBAction)button4{
+    [self editResult:@"4"];
+}
+- (IBAction)button5 {
+    [self editResult:@"5"];
+}
+
+- (IBAction)button6 {
+    [self editResult:@"6"];
+}
+
+- (IBAction)button7 {
+    [self editResult:@"7"];
+}
+
+- (IBAction)button8 {
+    [self editResult:@"8"];
+}
+
+- (IBAction)button9 {
+    [self editResult:@"9"];
+}
+
+- (IBAction)button0 {
+    [self editResult:@"0"];
+}
+
+- (IBAction)buttonEqual {
+}
+
+- (IBAction)buttonPlus {
+    [self editResult:@"+"];
+}
+
+- (IBAction)buttonMinus {
+    [self editResult:@"-"];
+}
+
+- (IBAction)buttonMult {
+    [self editResult:@"*"];
+}
+
+- (IBAction)buttonDiv {
+    [self editResult:@"/"];
+}
+
+- (IBAction)buttonC {
+    self.resultLabel.text = @"0";
+}
 @end
